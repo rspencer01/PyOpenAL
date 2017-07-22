4d39659c53db01dc2dc83000af90fd72be50fae7
Zuzu-Typ <Zuzu.Typ@gmail.com>
2017-07-22 20:37:41 +0200

    Remove old
    - tests and other stuff

--- tools/gen.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-{ cat _$1.start ; python conv.py _$1.proto $1; } | grep -v GetError.errcheck
--- tools/gengen.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-./gen.sh al > ../openal/_al.py
-./gen.sh alc > ../openal/_alc.py
-./gen.sh alut > ../openal/_alut.py
